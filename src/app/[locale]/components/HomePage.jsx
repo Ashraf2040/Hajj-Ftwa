cdd7e1f3cbef90082d2df9cf249edda7d78b08e5
Ashraf2040 <m_a199025@yahoo.com>
2024-03-31 12:25:02 +0300

    3

--- src/app/[locale]/components/HomePage.jsx
+++ src/app/[locale]/components/HomePage.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import PredefinedQuestions from "./PrefefinedQuestions";
 import { useLocale } from "next-intl";
 import Link from "next/link";
-
+import Loading from "../(fatwa)/fatwa/Loading";
 const FatwaPage = () => {
   const [suggesstions, setSuggestions] = useState([]);
   const [userInput, setUserInput] = useState("");
@@ -23,8 +23,6 @@ const FatwaPage = () => {
   useEffect(() => {
     getQuestions();
   }, []);
-
-  console.log(predefinedQuestions);
 
   const questionsToAsk = [];
   predefinedQuestions.forEach((element) => {
@@ -45,10 +43,10 @@ const FatwaPage = () => {
     setSuggestions(filtered);
   }, [userInput]);
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const questionAnswer =await predefinedQuestions.find(
+    const questionAnswer = await predefinedQuestions.find(
       (question) => question.question === userInput
     );
     setAnswer(questionAnswer.answer);
@@ -69,90 +67,105 @@ const FatwaPage = () => {
   const locale = useLocale();
   const url = `/${locale}/askai`;
 
+  const arrived = predefinedQuestions.length > 0;
+  if (arrived) {
+    console.log(predefinedQuestions);
+  } else {
+    console.log("not arrived");
+  }
+
   return (
     <div className=" ">
+
+      {!arrived ?<Loading />:
+      <>
+   
       <div className=" flex flex-col w-full   items-center  py-4   ">
-        <div className="navigate flex items-center  w-[90%] my-2">
-          <form
-            onSubmit={handleSubmit}
-            className=" items-center justify-center  w-full lg:w-2/5 "
-          >
-            <input
-              // disabled={status !== "awaiting_message"}
-              className="  p-4  border border-gray-300 bg-white  rounded shadow-xl w-4/5"
-              value={userInput}
-              placeholder="Type your question..."
-              type="text"
-              onChange={handleUserInputChange}
-
-              // onKeyDown={handleKeyDown}
-            />
-            <button className="py-4 bg-black rounded-r-lg  font-bold text-white px-2 -ml-2 ">
-              Submit
-            </button>
-          </form>
-        </div>
-
-        <div className="w-full  my-4 flex items-center justify-center  ">
-          {" "}
-          {userInput && (
-            <ul
-              className={`${
-                suggestionLinks
-                  ? " mx-4   grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4   "
-                  : "hidden"
-              }`}
-            >
-              {suggesstions.length > 0 &&
-                suggesstions.map((suggestion) => (
-                  <li
-                    key={suggestion}
-                    onClick={() => {
-                      setUserInput(suggestion);
-                      setSuggestionLinks(false);
-                    }}
-                    className="border-2 text-center rounded-lg py-2 sm:text-sm leading-6 font-semibold text-md shadow-md px-2  cursor-pointer "
-                  >
-                    {suggestion}
-                  </li>
-                ))}
-            </ul>
-          )}
-        </div>
-        <div
-          className={`answer w-4/5 rounded-lg relative bg-[#F0F4F9] p-8 mt-16 ${
-            answerBox ? "block" : "hidden"
-          }`}
+      <div className="navigate flex items-center  w-[90%] my-2">
+        <form
+          onSubmit={handleSubmit}
+          className=" items-center justify-center  w-full lg:w-2/5 "
         >
-          <span
-            className=" h-6 flex items-center justify-center w-6 absolute  text-black font-bold  right-1 top-1  bg-white    rounded-full cursor-pointer "
-            onClick={hanleCloseClick}
+          <input
+            // disabled={status !== "awaiting_message"}
+            className="  p-4  border border-gray-300 bg-white  rounded shadow-xl w-4/5"
+            value={userInput}
+            placeholder="Type your question..."
+            type="text"
+            onChange={handleUserInputChange}
+
+            // onKeyDown={handleKeyDown}
+          />
+          <button className="py-4 bg-black rounded-r-lg  font-bold text-white px-2 -ml-2 ">
+            Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="w-full  my-4 flex items-center justify-center  ">
+        {" "}
+        {userInput && (
+          <ul
+            className={`${
+              suggestionLinks
+                ? " mx-4   grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4   "
+                : "hidden"
+            }`}
           >
-            x
-          </span>
-          <p className="bg-[#CDE4D6] w-fit  px-4 py-3 md:font-semibold right-2  mb-8 rounded-lg">
-            <span className="md:text-2xl">🤵</span> {userInput}
-          </p>
-          <div className="bg-white rounded-lg sn:text-sm p-4 flex items-center gap-6">
-            <ReactTyped strings={[answer]} typeSpeed={40} />
-          </div>
+            {suggesstions.length > 0 &&
+              suggesstions.map((suggestion) => (
+                <li
+                  key={suggestion}
+                  onClick={() => {
+                    setUserInput(suggestion);
+                    setSuggestionLinks(false);
+                  }}
+                  className="border-2 text-center rounded-lg py-2 sm:text-sm leading-6 font-semibold text-md shadow-md px-2  cursor-pointer "
+                >
+                  {suggestion}
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
+      <div
+        className={`answer w-4/5 rounded-lg relative bg-[#F0F4F9] p-8 mt-16 ${
+          answerBox ? "block" : "hidden"
+        }`}
+      >
+        <span
+          className=" h-6 flex items-center justify-center w-6 absolute  text-black font-bold  right-1 top-1  bg-white    rounded-full cursor-pointer "
+          onClick={hanleCloseClick}
+        >
+          x
+        </span>
+        <p className="bg-[#CDE4D6] w-fit  px-4 py-3 md:font-semibold right-2  mb-8 rounded-lg">
+          <span className="md:text-2xl">🤵</span> {userInput}
+        </p>
+        <div className="bg-white rounded-lg sn:text-sm p-4 flex items-center gap-6">
+          <ReactTyped strings={[answer]} typeSpeed={40} />
         </div>
       </div>
-      <PredefinedQuestions
-        setUserInput={setUserInput}
-        handleSubmit={handleSubmit}
-        setSuggestions={setSuggestions}
-        setSuggestionLinks={setSuggestionLinks}
-      />
-      <h2 className="my-12 w-full text-center text-md text-red-500 font-semibold">
-        Cannot find your question?{" "}
-        <Link href={url}>
-          {" "}
-          <button className="px-2 bg-black text-white rounded-md ml-1 py-0.5 border-2 border-white">
-            Ask Ai
-          </button>
-        </Link>
-      </h2>
+    </div>
+    <PredefinedQuestions
+      setUserInput={setUserInput}
+      handleSubmit={handleSubmit}
+      setSuggestions={setSuggestions}
+      setSuggestionLinks={setSuggestionLinks}
+    />
+    <h2 className="my-12 w-full text-center text-md text-red-500 font-semibold">
+      Cannot find your question?{" "}
+      <Link href={url}>
+        {" "}
+        <button className="px-2 bg-black text-white rounded-md ml-1 py-0.5 border-2 border-white">
+          Ask Ai
+        </button>
+      </Link>
+    </h2>
+    </>
+    }
+  
+      
     </div>
   );
 };
